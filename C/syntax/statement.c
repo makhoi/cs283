b6dcf13727e0a09b5daf8a1556824f6f132d4f44
Khoi Ma <hoangkhoima2003@gmail.com>
2025-01-25 15:18:15 -0500

    statement

---
+++ C/syntax/statement.c
@@ -0,0 +1,10 @@
+//computer program = set of instructions to be executed
+//instructions = statements
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello World!\n");
+    printf("Have a good day!\n");
+    return 0;
+}
