583a2b3ab8e19da149e65a9b3009f126adc55f8d
Khoi Ma <hoangkhoima2003@gmail.com>
2025-01-26 16:10:32 -0500

    character lesson

--- C/datatypes/character.c
+++ C/datatypes/character.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 int main() {
+    //ASCII number to character
     char guessWhatCharacterDoes107Represent = 107;
     char guessWhatCharacterDoes104Represent = 104;
     char guessWhatCharacterDoes111Represent = 111;
@@ -12,6 +13,16 @@ int main() {
     printf("%c\n", guessWhatCharacterDoes104Represent);
     printf("%c\n", guessWhatCharacterDoes111Represent);
     printf("%c\n", guessWhatCharacterDoes105Represent);
+
+    // DONT use char type to store more than 1 character
+    // // generate error
+    // char myText = 'khoi';
+    // printf("%c\n", myText);
+
+    // use STRING to store multiple character - char type with %s format specifier
+    char myText [] = "khoi"; // syntax: [] after var name , 
+                            //wrapping around text must be double quote "" NOT single quote ''
+    printf("%s\n", myText);
 
     return 0;
 }
