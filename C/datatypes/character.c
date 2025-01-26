c87442a715eb85b7c66dc1a71b621887c7757812
Khoi Ma <hoangkhoima2003@gmail.com>
2025-01-26 16:00:20 -0500

    character and datatypes lession

---
+++ C/datatypes/character.c
@@ -0,0 +1,17 @@
+// character can be represented by number using ASCII table
+
+#include <stdio.h>
+
+int main() {
+    char guessWhatCharacterDoes107Represent = 107;
+    char guessWhatCharacterDoes104Represent = 104;
+    char guessWhatCharacterDoes111Represent = 111;
+    char guessWhatCharacterDoes105Represent = 105;
+
+    printf("%c\n", guessWhatCharacterDoes107Represent);
+    printf("%c\n", guessWhatCharacterDoes104Represent);
+    printf("%c\n", guessWhatCharacterDoes111Represent);
+    printf("%c\n", guessWhatCharacterDoes105Represent);
+
+    return 0;
+}
