c7064fa905f73beb477f8a361e56e892b9347ee8
Khoi Ma <hoangkhoima2003@gmail.com>
2025-01-26 14:58:48 -0500

    example

---
+++ C/variables/example.c
@@ -0,0 +1,16 @@
+// Example applied what we have learnt for variable section
+
+#include <stdio.h>
+
+int main() {
+    int studentID = 89, studentAge = 22;
+    char studentGrade = 'A';
+    float tuitionFee = 28000.76;
+
+    printf("Student ID: %d\n", studentID);
+    printf("Student Age: %d\n", studentAge);
+    printf("Student Grade: %c\n", studentGrade);
+    printf("Tuition Fee: %f\n", tuitionFee);
+
+    return 0;
+}
